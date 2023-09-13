a959ddc2c2935214255357c9537a0a15e73cc746
Defne <defne.keskin@etud.univ-jfc.fr>
2023-09-13 13:33:34 +0200

    Affichage Questionnaire et Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Page d'accueil
+  { path: 'questionnaire', component: QuestionnaireComponent },
+  // ... d'autres routes
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,27 @@ import { AppComponent } from './app.component';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionnaireComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: 'questionnaire',
+        component: QuestionnaireComponent
+      }
+    ])
   ],
   exports:[
     QuestionnaireComponent
