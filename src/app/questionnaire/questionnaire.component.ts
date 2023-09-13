18a88d14c0b86be656886c2547dff0e624d65fb8
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2023-09-13 14:56:51 +0200

    maj fonctionnel

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-questionnaire',
@@ -8,31 +7,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./questionnaire.component.css']
 })
 export class QuestionnaireComponent implements OnInit {
-  //regarder si un questionnaire dispo, si oui lafficher
-  apiUrlQuestionnaire = 'https://fhir.alliance4u.io/api/questionnaire/4';//'https://fhir.alliance4u.io/api/questionnaire?status=active';
+  apiUrlQuestionnaire = 'https://fhir.alliance4u.io/api/questionnaire/5';//'https://fhir.alliance4u.io/api/questionnaire?status=active';
   apiUrlReponse='https://fhir.alliance4u.io/api/questionnaire-response';
   apiData: any;
   idPatient="64f1fba11baf0c0018445640";
   questionnaireId="";
-  repondu:String[]=[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     
     this.http.get(this.apiUrlQuestionnaire).subscribe((data : any) => {
         let idMedecin= data.publisher;
+                
+        console.log(idMedecin);
 
-        if(this.repondu.includes(data.id)){
-          this.apiData="Pas de nouveau formulaire";
-        }else{
-          this.repondu.push(data.id);
+        this.apiData=data;
         
-          console.log(idMedecin);
-  
-  
-          this.apiData=data;
-        }
               
       });
   }
@@ -47,5 +38,6 @@ export class QuestionnaireComponent implements OnInit {
     let data="";
     this.http.post(this.apiUrlReponse, data);
     */
+
   } 
 }
