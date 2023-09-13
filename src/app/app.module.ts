5527ebbb37cc869fc86e6b7142019b69fb818835
Defne <defne.keskin@etud.univ-jfc.fr>
2023-09-13 14:26:55 +0200

    Maj IdQuestionnaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,27 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionnaireComponent,
-    HomeComponent,
+    
   ],
   imports: [
+    HomeComponent,
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+
     RouterModule.forRoot([
       {
         path: 'questionnaire',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 import { Router } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,CommonModule],
+}) 
+
+
 export class HomeComponent {
   constructor(private router: Router) {}
+  sousMenu: boolean = false;
 
   redirectToQuestionnaire() {
     this.router.navigate(['/questionnaire']);
   }
+  AfficheSousMenu(){
+    this.sousMenu =true;
+    console.log('coucocu')
+  }
+  
+  @Input()
+  color: ThemePalette;
 
 }
+
